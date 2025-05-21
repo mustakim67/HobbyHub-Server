0bfcb91516caf583919b4d2c41bab12c50dabef3
mustakim67-coder <mustakimapple95@gmail.com>
2025-05-21 18:03:37 +0600

    Connect Database to server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.SECRET_KEY}@cluster0.xqfap2z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const app=express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,28 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hobby server is running')
