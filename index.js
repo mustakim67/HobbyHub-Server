2bfd45974a88550174b84452d82edceda7705bf6
mustakim67-coder <mustakimapple95@gmail.com>
2025-05-24 13:04:19 +0600

    comment out unnecesary lines

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
      const UserCollection = client.db('UsersDB').collection('Users');
      const GroupsCollection = client.db('UsersDB').collection('Groups');
@@ -83,8 +83,8 @@ app.get('/groups/featured', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
